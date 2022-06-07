084311936474d3ce7a98bcdc144451ed7c3beea3
Conor McKenna <conormckenna63@yahoo.com>
2022-06-06 20:10:48 -0600

    Setup complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>TODOS</h1>
+        <ul>
+          <li>Go to the Gym</li>
+          <li>Do your Homework</li>
+          <li>Pay your Bills</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+        <button>Clear</button>
+
       </div>
     )
   }
