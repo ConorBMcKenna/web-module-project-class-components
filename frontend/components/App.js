ae1758a07c4b3441205f2dbcb910f83e44b27eb0
Conor McKenna <conormckenna63@yahoo.com>
2022-06-06 20:37:56 -0600

    Added One Component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,40 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286, 
+          completed: true
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: true
+        },
+        {
+          task: 'Go to Gym',
+          id: 1528817084358,
+          completed: true
+        },
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state
     return (
       <div>
         <h1>TODOS</h1>
         <ul>
-          <li>Go to the Gym</li>
-          <li>Do your Homework</li>
-          <li>Pay your Bills</li>
+          {
+            todos.map(todo=> {
+              return (<li>{todo.task} { todo.completed ? <span> completed </span> : <span></span>}</li>)
+            })
+          }
+          
         </ul>
 
         <form>
